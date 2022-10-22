9967e663021924acee9a96954e10f2b8896b26e7
CR790 <cristianrenz56@gmail.com>
2022-10-21 23:11:35 -0300

    cambios

--- PL/settings.py
+++ PL/settings.py
@@ -76,9 +76,14 @@ WSGI_APPLICATION = 'PL.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'prueba', 
+        'USER': 'postgres', 
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
+    
 }
 
 
--- app/forms.py
+++ app/forms.py
@@ -4,9 +4,13 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.forms import ModelForm, widgets
 from django.contrib.auth.models import User
+from app.models import Nombre
  
-class CrearProfesor(forms.Form):
+class CrearProfesor(ModelForm):
     fname= forms.CharField(label="Nombre", max_length=255, min_length=3, required=True)
     lname= forms.CharField(label="Apellido", max_length=255, min_length=3, required=True)
    
+    class Meta:
+        model = Nombre
+        fields = '__all__'
 
